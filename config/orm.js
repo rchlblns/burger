7df5c06493c9d6454cb5888e7601b37676adcbff
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-11-17 19:56:27 -0600

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,63 @@
+//Imports MySQL connection
 const connection = require("connection.js");
 
+const orm = {
+    
+    selectAll: function(tableInput, cb){
+        var queryString = "SELECT * FROM" + tableInput + ";";
 
+        connection.query(queryString, function(err, result){
+
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+
+        var queryString = "INSERT INTO " + table;
+
+        queryString += "(";
+        queryString += cols.toString();
+        queryString += ")";
+        queryString += "VALUES (";
+        queryString += ")";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result){
+
+            if (err){
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: function(table, objColVals, condition, cb){
+
+        var queryString = "UPDATE " + table;
+
+        queryString += "SET ";
+        queryString += objColVals;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+
+            if (err){
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+//Exports the orm object
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,17 @@
 const express = require("express");
+
+const router = express.Router();
+
+//imports the burgers.js model
+const burger = require("../models/burger.js");
+
+
+router.get("/", function(req,res){
+    burger.all(function (data){
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
